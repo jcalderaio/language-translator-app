bc4b204cfb3f65957354ad4bcfeb189a9002faad
John Calderaio <jcalderaio@gmail.com>
2017-10-26 17:16:26 -0400

    first push

---
+++ src/Language.java
@@ -0,0 +1,75 @@
+import java.util.*;
+/**
+ * List of all the languages accepted by the Google Translate API and the 
+ * respective string used to access the web service.
+ * @author Diego
+ */
+public class Language { 
+    
+   public static final String Afrikaans = "af";
+public static final String Albanian = "sq";
+public static final String Arabic = "ar";
+public static final String Azerbaijani = "az";
+public static final String Basque = "eu";
+public static final String Bengali = "bn";
+public static final String Belarusian = "be";
+public static final String Bulgarian = "bg";
+public static final String Catalan = "ca";
+public static final String ChineseSimplified = "zh-CN";
+public static final String ChineseTraditional = "zh-TW";
+public static final String Croatian = "hr";
+public static final String Czech = "cs";
+public static final String Danish = "da";
+public static final String Dutch = "nl";
+public static final String English = "en";
+public static final String Esperanto = "eo";
+public static final String Estonian = "et";
+public static final String Filipino = "tl";
+public static final String Finnish = "fi";
+public static final String French = "fr";
+public static final String Galician = "gl";
+public static final String Georgian = "ka";
+public static final String German = "de";
+public static final String Greek = "el";
+public static final String Gujarati = "gu";
+public static final String HaitianCreole = "ht";
+public static final String Hebrew = "iw";
+public static final String Hindi = "hi";
+public static final String Hungarian = "hu";
+public static final String Icelandic = "is";
+public static final String Indonesian = "id";
+public static final String Irish = "ga";
+public static final String Italian = "it";
+public static final String Japanese = "ja";
+public static final String Kannada = "kn";
+public static final String Korean = "ko";
+public static final String Latin = "la";
+public static final String Latvian = "lv";
+public static final String Lithuanian = "lt";
+public static final String Macedonian = "mk";
+public static final String Malay = "ms";
+public static final String Maltese = "mt";
+public static final String Norwegian = "no";
+public static final String Persian = "fa";
+public static final String Polish = "pl";
+public static final String Portuguese = "pt";
+public static final String Romanian = "ro";
+public static final String Russian = "ru";
+public static final String Serbian = "sr";
+public static final String Slovak = "sk";
+public static final String Slovenian = "sl";
+public static final String Spanish = "es";
+public static final String Swahili = "sw";
+public static final String Swedish = "sv";
+public static final String Tamil = "ta";
+public static final String Telugu = "te";
+public static final String Thai = "th";
+public static final String Turkish = "tr";
+public static final String Ukrainian = "uk";
+public static final String Urdu = "ur";
+public static final String Vietnamese = "vi";
+public static final String Welsh = "cy";
+public static final String Yiddish = "yi";
+
+    
+}
